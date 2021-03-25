f94ad1e1e8daeef523db176ef6627554ac2c7e71
Kimmiich <kimmielundgren@live.se>
2021-03-25 16:18:39 +0100

    added about me text

--- modules/printFunctions.mjs
+++ modules/printFunctions.mjs
@@ -14,9 +14,8 @@ function printProfile(main) {
     main.insertAdjacentHTML("beforeend", `
     <section id="startPage">
       <h1>Kimmie Lundgren</h1>
-      <p>Front-end Developer 2022</p>
-      <i class="fas fa-arrow-down"></i>
-</section>`
+      <p class="secondTitle">Front-end Developer 2022</p>
+    </section>`
 )};
 
 function printAboutMe(main) { 
@@ -25,7 +24,7 @@ function printAboutMe(main) {
     <section id="aboutMePage">
     <article>
         <h2>About me</h2>
-        <p>antingen text eller li</p>/<li></li>
+        <p>I´m 28 years old and live in Norrtälje. Right now I am studying to become a Front-End development with an expected exam in 2022. My goal with the education is to gain new knowledge in a new industry and hopefully find a new career. Something I value in programming is that you get to challenge yourself in every project and that you are constantly learning new things. What I add at work is excellent structure, curiosity and positive energy.</p>
     </article>
     </section>`
 )};
